e242b2bdfc4772354a1893761d9ee6096987c19c
Rodrigo <rcoto@protonmail.com>
2019-02-18 23:17:52 -0800

    04 Pig Latin Completed it

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -8,7 +8,9 @@ def translate(str)
   word_phrase.each do |word|
     if ["a","e","i","o","u"].include?(word[0].downcase)
       final_phrase += word + "ay "
-    elsif ["sm", "st", "gl", "tr", "fl", "th", "sc", "qu", "sq", "br"].include?(word[0..1].downcase)
+    elsif ["thr", "sch", "squ"].include?(word[0..2].downcase)
+      final_phrase += word[3..-1] + word[0..2] + "ay "
+    elsif ["sm", "ch", "st", "gl", "tr", "fl", "th", "sc", "qu", "sq", "br"].include?(word[0..1].downcase)
       final_phrase += word[2..-1] + word[0..1] + "ay "
 
     elsif ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'y', 'z'].include?(word[0].downcase)
@@ -17,5 +19,5 @@ def translate(str)
       final_phrase += word
     end 
   end
-  return final_phrase
+  return final_phrase.rstrip
 end
