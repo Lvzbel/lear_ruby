97e54363262bc761de55cb799249117f41493682
Rodrigo <rcoto@protonmail.com>
2019-02-22 23:59:01 -0800

    06 Time in Progress

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,26 @@
 class Timer
-  #write your code here
+  def initialize
+    @timer = Time.new(0000, 1, 1, 0, 0, 0,)
+  end
+
+  def seconds=(seconds)
+    @timer = Time.at(seconds)
+  end
+
+  def seconds
+    @timer = Time.at(0)
+  end
+
+  def show
+    puts @timer
+  end
+  
 end
+
+test = Timer.new
+test.show
+test.seconds = 66
+test.show
+test.seconds
+test.show
+
